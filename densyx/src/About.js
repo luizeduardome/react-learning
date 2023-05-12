4b76a9fa5e22352ccae81d13b123691ed5d57a8b
Luiz Eduardo <luizefm@al.insper.edu.br>
2023-05-12 14:13:43 -0300

    Exercicio 1 finalizado e corrigido.

---
+++ densyx/src/About.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+class About extends Component {
+    render() {
+        return(
+            <div>
+                <h1>
+                    Luiz Eduardo de Figueiredo Menezes
+                </h1>
+                <p>
+                    Fascinado por tecnologia e e por programação de software.
+                </p>
+                <h2>
+                    Minhas Habilidades
+                </h2>
+                <ul>
+                    <li>React</li>
+                    <li>Python</li>
+                    <li>Git</li>
+                    <li>JavaScript</li>
+                    <li>Docker</li>
+                    <li>SQL</li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default About;
